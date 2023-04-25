b1c7494c008efde45419db9a8b18eed2aec32b50
Honey Patel <honey.p@simformsolutions.com>
2023-04-25 09:49:39 +0530

    complete task image changes

--- src/Components/NoTask.jsx/NoTask.jsx
+++ src/Components/NoTask.jsx/NoTask.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './NoTask.module.css'
+import completeTask from '../../assets/completeTask.png'
 
 
 function NoTask() {
     return (
 
         <div className={styles.noTaskDiv}>
-            <h2 className={styles.noTaskHeading}>Done for Today</h2>
-            <img src='../../assets/completeTask.png' alt="" height="250px" />
+            <h2 className={styles.noTaskHeading}>No Task for Today</h2>
+            <img src={completeTask} alt="" height="250px" />
         </div>
 
     )
