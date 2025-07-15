8783d39352a1642ec42eb641d616d3a2b7342705
Honey Patel <honey.p@simformsolutions.com>
2025-07-15 19:28:15 +0530

    feat: add TODO

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         onChange={(e) => setInput(e.target.value)}
         placeholder="Add a task"
       />
-      <button onClick={addTask}>Add</button>
+      <button onClick={addTask}>Add TODO</button>
       <ul>
         {tasks.map((task, idx) => (
           <li key={idx}>
